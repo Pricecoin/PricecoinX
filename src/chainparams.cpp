244b49d560b134ed6d45e04e819a29b34473fc90
ZachPrice <zach.price105@gmail.com>
2023-12-28 15:02:39 -0600

    Initial Port Change

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -108,7 +108,7 @@ public:
         pchMessageStart[1] = 0xc0;
         pchMessageStart[2] = 0xb6;
         pchMessageStart[3] = 0xdb;
-        nDefaultPort = 9333;
+        nDefaultPort = 2333;
         nPruneAfterHeight = 100000;
         m_assumed_blockchain_size = 40;
         m_assumed_chain_state_size = 2;
