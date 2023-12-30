3d758d8d654c7f1dbe417aa58499f37dfb2f1092
Zach Price <37559274+zachchan105@users.noreply.github.com>
2023-12-30 12:39:12 -0600

    Revert "block reward validation.cpp"
    This reverts commit 0c0b09371fb372d66f429854ce06f563f7496cd5.

--- src/validation.cpp
+++ src/validation.cpp
@@ -1158,8 +1158,8 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
     if (halvings >= 64)
         return 0;
 
-    CAmount nSubsidy = 25 * COIN;
-    // Subsidy is cut in half every x blocks which will occur approximately every x years.
+    CAmount nSubsidy = 50 * COIN;
+    // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
     nSubsidy >>= halvings;
     return nSubsidy;
 }
